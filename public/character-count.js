d22e508f5bd71d236428cef133983f55b5b13125
Sam Blackman <sblackman@gmail.com>
2014-10-16 13:45:19 -0400

    add basic character counter that turns red over 140

--- public/character-count.js
+++ public/character-count.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+  $("textarea").keydown(function() {
+    $("textarea").keydown(function() {
+      updateCharacterCount.bind(this)();
+    });
+  })
 });
+
+function updateCharacterCount() {
+  var textColor = "black";
+  var charCount = $(this).val().length;
+  $(".char-count").html(charCount);
+  if (charCount > 140) {
+    textColor = "red"
+  } else {
+    textColor = "black"
+}
+  $(".char-count").css("color", textColor);
+}
