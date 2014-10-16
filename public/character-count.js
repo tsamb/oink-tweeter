f32a3f26914468f0785fd705bdb4e165bf3a4d29
Sam Blackman <sblackman@gmail.com>
2014-10-16 15:09:08 -0400

    refactor char count js

--- public/character-count.js
+++ public/character-count.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
-  $("textarea").keydown(function() {
+  $("textarea").keyup(function() {
     updateCharacterCount.bind(this)();
   });
 });
 
 function updateCharacterCount() {
-  var textColor = "black";
   var charCount = $(this).val().length;
   $(".char-count").html(charCount);
   if (charCount > 140) {
+    overLimit(true);
+  } else {
+    overLimit(false);
+  }
+}
+
+function overLimit(pastLimit) {
+  var textColor, buttonDisabled
+  if (pastLimit) {
     textColor = "red";
+    buttonDisabled = true;
   } else {
     textColor = "black";
-}
+    buttonDisabled = false;
+  }
   $(".char-count").css("color", textColor);
+  $("input[type='submit']").prop("disabled", buttonDisabled);
 }
